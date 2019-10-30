83674251271a0f05b87babb5ee7fef43ee2e410e
Krzysztof <krrsssfire@gmail.com>
2019-10-30 21:13:55 +0100

    added admins table in sql script (to consider: keep all users and admins in one table or separate tables) also merged two scripts into one

--- sql-scripts/setup-db-internhip-survey.sql
+++ sql-scripts/setup-db-internhip-survey.sql
@@ -16,20 +16,33 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Table structure for table `admins`
 --
--- NOTE: The passwords are encrypted using BCrypt
---
--- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE `admins` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone_number` int NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 --
 -- Default passwords here are: fun123
 --
 
+
 INSERT INTO `users` 
 VALUES 
 ('john','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
-('mary','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1),
+('mary','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,),
 ('susan','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1);
+
 
 
 --
@@ -54,4 +67,25 @@ VALUES
 ('mary','ROLE_OILADMIN'),
 ('susan','ROLE_ADMIN');
 
+--
+-- Table structure for table `main_section`
+--
+
+DROP TABLE IF EXISTS `main_section`;
+CREATE TABLE `main_section` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `status` varchar(15) DEFAULT 'draft',
+  `medical_chamber` varchar(50),
+  `unit_name` varchar(50) NOT NULL,
+                                                                                                                                                                                             
+  
+  PRIMARY KEY `username_idx_1` (`id`),
+  CONSTRAINT `username_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO main_section
+(username, medical_chamber, unit_name) 
+values ('john', 'OIL w Szczecinie', 'SPWSZ'),
+	   ('mary', 'OIL w Szczecinie', 'SPSK1');
 
