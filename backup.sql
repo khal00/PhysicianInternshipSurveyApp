d383c45668a590d895ddcd9f8ca6db6ca75d4456
krzysztof <krzhalewski@gmail.com>
2020-04-10 15:23:09 +0200

    few css changes

--- backup.sql
+++ backup.sql
@@ -14,15 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `internship_survey`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `internship_survey` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
-
-USE `internship_survey`;
-
 --
 -- Table structure for table `acl_class`
 --
